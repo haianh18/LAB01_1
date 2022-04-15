2de5fe926da2b66403d26aa5a492ba04febc53e2
Hai Anh Lai Vu <laivuhaianh18@gmail.com>
2022-04-15 11:27:40 +0700

    fifth commit

--- src/components/StaffComponents.js
+++ src/components/StaffComponents.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import dateFormat from 'dateformat';
 
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardText, CardBody, CardTitle } from 'reactstrap';
 class StaffList extends Component {
     constructor(props) {
         super(props);
